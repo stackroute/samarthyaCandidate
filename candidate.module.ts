5a0e14dfbab5f4ab0f498b06e4c352b13f2d6222
sankhlasaini <sankhlasaini@gmail.com>
2017-04-04 22:56:12 +0530

    services moved to webcomponent

--- candidate.module.ts
+++ candidate.module.ts
@@ -1,12 +1,15 @@
 import { MainComponent } from './mainComponent';
+import { ModuleWithProviders } from '@angular/core';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
+// local services
 import { UserService } from './services/user.service';
-import { ProfileService } from './services/profile.service';
 import { JsonDataService } from './services/json-data.service';
 import { EmailService } from './services/email.service';
 import { Data } from './services/data.service';
 import { AuthenticationService } from './services/authentication.service';
-import { AppRoutingModule, routingComponents } from './app-routing.module';
-import { ModuleWithProviders } from '@angular/core';
+
+// local components
 import { VerifyEmailComponent } from './components/loginComponent/verify-email/verify-email.component';
 import { PasswordResetComponent } from './components/loginComponent/password-reset/password-reset.component';
 import { LoginHeaderComponent } from './components/loginComponent/login-header/login-header.component';
@@ -21,9 +24,7 @@ import { EmployersComponent } from './components/employers/employers.component';
 import { CandidateRegisterComponent } from './components/loginComponent/candidate-register/candidate-register.component';
 import { CandidateSearchComponent } from './components/candidate-search/candidate-search.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-
 import { PersonalInfoForm } from './components/profileSectionForm/personalInfoForm/personalInfoForm.component';
-
 import { NgModule } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { HttpModule } from '@angular/http';
@@ -32,85 +33,86 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { Md2Module } from 'md2';
 import { SamarthyaWebComponentsModule } from 'samarthyaWebcomponent';
-import { SamProfileCardService } from './services/sam-profile-card.service';
-import { SamProfileSectionConfigService } from './services/sam-profile-section-config.service';
+import { SamProfileCardService } from 'samarthyaWebcomponent/sam-profile/sam-profile-card/sam-profile-card.service';
+import { SamProfileSectionConfigService } from 'samarthyaWebcomponent/sam-profile/sam-profile-section/sam-profile-section-config.service';
+import { ProfileService } from 'samarthyaWebcomponent/sam-profile/sam-profile-section/sam-profile-section-data.service';
 import { SamProfileSectionFormComponent } from 'samarthyaWebcomponent/sam-profile/sam-profile-section-form/sam-profile-section-form.component';
 import { SamProfileSectionComponent } from 'samarthyaWebcomponent/sam-profile/sam-profile-section/sam-profile-section.component';
 import { SamProfileCardComponent } from 'samarthyaWebcomponent/sam-profile/sam-profile-card/sam-profile-card.component';
 
 @NgModule({
-	imports: [
-		BrowserModule,
-		FormsModule,
-		HttpModule,
-		MaterialModule.forRoot(),
-		Md2Module.forRoot(),
-		ReactiveFormsModule,
-		FlexLayoutModule,
-		AppRoutingModule,
-		SamarthyaWebComponentsModule,
-	],
-	declarations: [
-		AboutUsComponent,
-		CandidateSearchComponent,
-		CandidateRegisterComponent,
-		DashboardComponent,
-		EmployersComponent,
-		EventPostComponent,
-		JobPostComponent,
-		LayoutComponent,
-		LoginComponent,
-		CandidateRegisterComponent,
-		ForgotPasswordComponent,
-		LoginFooterComponent,
-		LoginHeaderComponent,
-		PasswordResetComponent,
-		VerifyEmailComponent,
-		MainComponent,
-		routingComponents,
-		PersonalInfoForm
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule.forRoot(),
+    Md2Module.forRoot(),
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    AppRoutingModule,
+    SamarthyaWebComponentsModule,
+  ],
+  declarations: [
+    AboutUsComponent,
+    CandidateSearchComponent,
+    CandidateRegisterComponent,
+    DashboardComponent,
+    EmployersComponent,
+    EventPostComponent,
+    JobPostComponent,
+    LayoutComponent,
+    LoginComponent,
+    CandidateRegisterComponent,
+    ForgotPasswordComponent,
+    LoginFooterComponent,
+    LoginHeaderComponent,
+    PasswordResetComponent,
+    VerifyEmailComponent,
+    MainComponent,
+    routingComponents,
+    PersonalInfoForm
 
-	],
-	bootstrap: [MainComponent, PersonalInfoForm],
-	exports: [
-		AboutUsComponent,
-		CandidateSearchComponent,
-		CandidateRegisterComponent,
-		DashboardComponent,
-		EmployersComponent,
-		EventPostComponent,
-		JobPostComponent,
-		LayoutComponent,
-		LoginComponent,
-		CandidateRegisterComponent,
-		ForgotPasswordComponent,
-		LoginFooterComponent,
-		LoginHeaderComponent,
-		PasswordResetComponent,
-		VerifyEmailComponent,
-		MainComponent,
-		routingComponents,
-		SamProfileCardComponent,
-		SamProfileSectionComponent,
-		SamProfileSectionFormComponent,
-		PersonalInfoForm
-	]
+  ],
+  bootstrap: [MainComponent, PersonalInfoForm],
+  exports: [
+    AboutUsComponent,
+    CandidateSearchComponent,
+    CandidateRegisterComponent,
+    DashboardComponent,
+    EmployersComponent,
+    EventPostComponent,
+    JobPostComponent,
+    LayoutComponent,
+    LoginComponent,
+    CandidateRegisterComponent,
+    ForgotPasswordComponent,
+    LoginFooterComponent,
+    LoginHeaderComponent,
+    PasswordResetComponent,
+    VerifyEmailComponent,
+    MainComponent,
+    routingComponents,
+    SamProfileCardComponent,
+    SamProfileSectionComponent,
+    SamProfileSectionFormComponent,
+    PersonalInfoForm
+  ]
 })
 
 export class CandidateModule {
-	static forRoot(): ModuleWithProviders {
-		return {
-			ngModule: CandidateModule,
-			providers: [
-				AuthenticationService,
-				Data,
-				EmailService,
-				JsonDataService,
-				ProfileService,
-				UserService,
-				SamProfileCardService,
-				SamProfileSectionConfigService,
-			]
-		}
-	}
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CandidateModule,
+      providers: [
+        AuthenticationService,
+        Data,
+        EmailService,
+        JsonDataService,
+        ProfileService,
+        UserService,
+        SamProfileCardService,
+        SamProfileSectionConfigService,
+      ]
+    }
+  }
 }
