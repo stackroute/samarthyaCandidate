96f792d6aa7246d1f3f697aa4b4f690aa5a9383e
prakulkumar <dhiman404prakul@gamil.com>
2017-04-05 14:08:38 +0530

    fixe qualification

--- components/profileSectionForm/qualificationForm/qualificationForm.component.ts
+++ components/profileSectionForm/qualificationForm/qualificationForm.component.ts
@@ -24,37 +24,34 @@ export class QualificationForm implements OnInit {
     public dialogRef: MdDialogRef<QualificationForm>
   ) {
     this.userForm = fb.group({
-      name: [this.dialogRef.config.data.name, [Validators.required]],
-      subject: [this.dialogRef.config.data.subject, [Validators.required]],
-      academictype: [this.dialogRef.config.data.academictype, [Validators.required]],
-      batch: [this.dialogRef.config.data.batch, [Validators.required]],
-      result: [this.dialogRef.config.data.result, [Validators.required]],
-      institute: [this.dialogRef.config.data.institute, [Validators.required]],
-      affiliation: [this.dialogRef.config.data.affiliation, [Validators.required]],
-      location: [this.dialogRef.config.data.location, [Validators.required, Validators.pattern('^[a-zA-Z\\s]*$')]],
+      name: [this.dialogRef.config.data[0].name, [Validators.required]],
+      subject: [this.dialogRef.config.data[0].subject, [Validators.required]],
+      academictype: [this.dialogRef.config.data[0].academictype, [Validators.required]],
+      batch: [this.dialogRef.config.data[0].batch.substring(0, 10), [Validators.required]],
+      result: [this.dialogRef.config.data[0].result, [Validators.required]],
+      institute: [this.dialogRef.config.data[0].institute, [Validators.required]],
+      affiliation: [this.dialogRef.config.data[0].affiliation, [Validators.required]],
+      location: [this.dialogRef.config.data[0].location, [Validators.required, Validators.pattern('^[a-zA-Z\\s]*$')]],
     });
   }
 
   ngOnInit() {
-    console.log(this.dialogRef.config.data);
+    console.log(this.dialogRef.config.data[0].batch.substring(0, 10) + " sjndjsdnjsnds");
   }
 
   onSave() {
-    let qualificationData = this.dialogRef.config.data;
-    qualificationData.name = this.userForm.value.name;
-    qualificationData.subject = this.userForm.value.subject;
-    qualificationData.academictype = this.userForm.value.academictype;
-    qualificationData.batch = this.userForm.value.batch;
-    qualificationData.result = this.userForm.value.result;
-    qualificationData.institute = this.userForm.value.institute;
-    qualificationData.affiliation = this.userForm.value.affiliation;
-    qualificationData.location = this.userForm.value.location;
+    console.log(this.dialogRef.config.data + 'save');
+    let qualificationData = {};
+    qualificationData['name'] = this.userForm.value.name;
+    qualificationData['subject'] = this.userForm.value.subject;
+    qualificationData['academictype'] = this.userForm.value.academictype;
+    qualificationData['batch'] = this.userForm.value.batch;
+    qualificationData['result'] = this.userForm.value.result;
+    qualificationData['institute'] = this.userForm.value.institute;
+    qualificationData['affiliation'] = this.userForm.value.affiliation;
+    qualificationData['location'] = this.userForm.value.location;
 
     let username = JSON.parse(localStorage.getItem('currentUser'))['username'];
-    console.log(qualificationData.name)
-    let q: any[] = [];
-    q.push(qualificationData);
-    console.log(q[0]);
     this.http.patch('/profile', { data: qualificationData, username: username, sectionName: 'qualifications' })
       .subscribe((response: Response) => {
         console.log('-->', response['_body']);
